8d2cc4693ad48009131e7bc462130018044fa205
abd-khadher1234 <112934007+abd-khadher1234@users.noreply.github.com>
2022-09-27 00:39:41 +0530

    fix

--- plugins/owner.js
+++
@@ -1,8 +0,0 @@
-const {Function,isPublic,Menu,RandomFancy,prefix} = require('../lib/');
-const {BOT_INFO} = require('../config')
-
-Function({pattern: 'owner ?(.*)', fromMe: isPublic, dontAddCommandList: false, type: 'info'}, async (m, text, client) => {
-const await client.sendMessage(m.chat, {
-templateButtons: [{urlButton: {displayText: 'Whatsapp group', url: 'https://chat.whatsapp.com/J2s0xRg4kZJ3ep0AncmbTJ'}},{urlButton: {displayText: 'Contact Owner', url: `https://wa.me/${BOT_INFO.split(";")[2]}`}},{quickReplyButton: {displayText: 'Menu', id: prefix + 'menu'}}]
-})
-});
