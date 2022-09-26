59045fc9f5c55481b79a7e73da92d432034ac697
abd-khadher1234 <112934007+abd-khadher1234@users.noreply.github.com>
2022-09-27 00:31:06 +0530

    retest

--- plugins/owner.js
+++ plugins/owner.js
@@ -2,8 +2,7 @@ const {Function,isPublic,Menu,RandomFancy,prefix} = require('../lib/');
 const {BOT_INFO} = require('../config')
 
 Function({pattern: 'owner ?(.*)', fromMe: isPublic, dontAddCommandList: false, type: 'info'}, async (m, text, client) => {
-const message= await RandomFancy(await Menu(m))
-await client.sendMessage(m.chat, {
+const await client.sendMessage(m.chat, {
 templateButtons: [{urlButton: {displayText: 'Whatsapp group', url: 'https://chat.whatsapp.com/J2s0xRg4kZJ3ep0AncmbTJ'}},{urlButton: {displayText: 'Contact Owner', url: `https://wa.me/${BOT_INFO.split(";")[2]}`}},{quickReplyButton: {displayText: 'Menu', id: prefix + 'menu'}}]
 })
 });
