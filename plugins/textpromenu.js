9705c815cf38201fe99017a373c6570c3ec42786
abd-khadher1234 <112934007+abd-khadher1234@users.noreply.github.com>
2022-09-30 17:35:19 +0530

    text pro menu

---
+++ plugins/textpromenu.js
@@ -0,0 +1,41 @@
+const { Function, Imgur, deleteFromImgur, isPublic} = require ('../lib/')
+
+Function (
+	{
+		pattern: 'textpro ?(.*)',
+		fromMe: isPublic,
+		desc: 'get text promenu',
+		type: 'textpro',
+	},async (message, match) => { 
+let msg = '```'
+await message.send(msg +````═════════════════════════════
+ⓣⓔⓧⓣⓟⓡⓞ ⓜⓔⓝⓤ ⓒⓞⓜⓜⓐⓝⓢ
+═════════════════════════════
+
+
+ 𝟷  HOORROR
+ 𝟸  WHITEBEAR
+ 𝟹  THUNDER
+ 𝟺  BLACKPINK
+ 𝟻  NEON
+ 𝟼  MATRIX
+ 𝟽  SKY
+ 𝟾  JOKER
+ 𝟿  MAGMA
+ 𝟷𝟶 SAND
+ 𝟷𝟷 GRAFFITTI
+ 𝟷𝟸 METALLIC
+ 𝟷𝟹 STEEL
+ 𝟷𝟺 HARRYPOTTER
+ 𝟷𝟻 UNDERWATER
+ 𝟷𝟼 LUXURY
+ 𝟷𝟽 GLUE
+ 𝟷𝟾 FABRIC
+ 𝟷𝟿 NEONLIGHT
+ 𝟸𝟶 LAVA
+ 𝟸𝟷 TOXIC
+ 𝟸𝟸 ANCIENT
+ 
+ 
+
+═════════════════════════════ ``` ` + '```',{quoted: message.data});});
