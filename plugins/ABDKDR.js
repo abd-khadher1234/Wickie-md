0a79a36e570ece1e4bb27cb2c3f791eaa5e7fde3
abd-khadher1234 <112934007+abd-khadher1234@users.noreply.github.com>
2022-10-16 11:04:40 +0530

    tempororyy deleted

--- plugins/ABDKDR.js
+++
@@ -1,5 +0,0 @@
-const { Function, isPublic, sendAlive, RandomFancy, runtime, chatBot, isChatBot, chatbot } = require("../lib/");
-const config = require('../config')
-Function({pattern: 'chatbot ?(.*)', fromMe: true, desc: 'set chat bot', type: 'misc'}, async (message, match) => {
-await chatBot(message, match)
-});
