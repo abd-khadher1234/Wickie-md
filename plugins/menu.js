1177ec8be5470645598f6ae4f28d9d72cd09c6af
abd-khadher1234 <112934007+abd-khadher1234@users.noreply.github.com>
2022-10-22 23:15:09 +0530

    menu change

--- plugins/menu.js
+++ plugins/menu.js
@@ -35,7 +35,7 @@ Function({
 			if (/\[(\W*)\]/.test(PREFIX)) {
 				HANDLER = PREFIX.match(/\[(\W*)\]/)[1][0];
 			} else {
-				HANDLER = '🤌🏻';
+				HANDLER = '.';
 			}
 			if (!commandslist[command.type]) commandslist[command.type] = []
 			commandslist[command.type].push((match.length >= 3 ? (HANDLER + mmatch) : command.pattern).trim())
